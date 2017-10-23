4a9f7d9a067ebbe9c3cc8e6ea71f0d6b101ef4b1
Hugo Bravo Diaz <hbravod@gamma20.aulas.gsyc.urjc.es>
2017-10-23 15:45:42 +0200

    ejer6

--- server.py
+++ server.py
@@ -14,25 +14,33 @@ if len(sys.argv) < 2:
     print("Usage: pyhton3 server.py PORT")
 
 
-class EchoHandler(socketserver.DatagramRequestHandler):
+class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
+
+    dic_client = {}
 
     def handle(self):
         """
         handle method of the server class
         (all requests will be handled by this method)
         """
-        self.wfile.write(b"Hemos recibido tu peticion")
+        self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
         print(self.client_address)
         for line in self.rfile:
-            print("El cliente nos manda ", line.decode('utf-8'))
+            if line:
+                if line.decode('utf-8')[:8] == 'REGISTER':
+                    print("El cliente nos manda:", line.decode('utf-8'))
+                    ip_user = line.decode('utf-8')[13:-10]
+                    self.dic_client[ip_user] = self.client_address[0]
+        print(self.dic_client)
+                
 
 if __name__ == "__main__":
     # Listens at localhost ('') port 6001 
     # and calls the EchoHandler class to manage the request
-    serv = socketserver.UDPServer(('', PORT), EchoHandler) 
+    serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler) 
 
     print("Lanzando servidor UDP de eco...")
     try:
