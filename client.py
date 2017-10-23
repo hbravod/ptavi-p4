222b7b8eed92f3224f0bdb667549ac7a1ec8c93e
Hugo Bravo Diaz <hbravod@gamma20.aulas.gsyc.urjc.es>
2017-10-23 16:59:11 +0200

    ejer7

--- client.py
+++ client.py
@@ -8,20 +8,21 @@ import socket
 import sys
 
 
-if len(sys.argv) < 4:
-    print("Usage: pyhton3 client.py SERVER PORT LINE")
+if len(sys.argv) < 5:
+    print("Usage: client.py ip puerto register sip_address expires_value")
 
 # Constantes. Dirección IP del servidor y contenido a enviar
-SERVER = sys.argv[1] #'localhost'
-PORT = int(sys.argv[2]) #puerto
-LINE = ' '.join(sys.argv[4:]) #'¡Hola mundo!'
+SERVER = sys.argv[1]
+PORT = int(sys.argv[2])
+LINE = sys.argv[4]
+EXPIRE = sys.argv[5]
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((SERVER, int(PORT)))
+    my_socket.connect((SERVER, PORT))
     print("Enviando:", LINE)
     if sys.argv[3] == "register":
-        my_socket.send(bytes('REGISTER sip:'+LINE +' SIP/2.0\r\n\r\n',                                                                                          'utf-8') + b'\r\n')
+        my_socket.send(bytes('REGISTER sip:'+LINE+' SIP/2.0\r\nExpires: '+EXPIRE+'\r\n','utf-8') + b'\r\n')
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
