3e1c404e8992acec2bb89c9872c143d6f97d9fc2
Hugo Bravo Diaz <hbravod@gamma20.aulas.gsyc.urjc.es>
2017-10-23 15:47:07 +0200

    ejer6

--- client.py
+++ client.py
@@ -7,16 +7,21 @@ Programa cliente UDP que abre un socket a un servidor
 import socket
 import sys
 
+
+if len(sys.argv) < 4:
+    print("Usage: pyhton3 client.py SERVER PORT LINE")
+
 # Constantes. Dirección IP del servidor y contenido a enviar
 SERVER = sys.argv[1] #'localhost'
-PORT = int(sys.argv[2]) #6001
-LINE = ' '.join(sys.argv[3:]) #'¡Hola mundo!'
+PORT = int(sys.argv[2]) #puerto
+LINE = ' '.join(sys.argv[4:]) #'¡Hola mundo!'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, int(PORT)))
     print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    if sys.argv[3] == "register":
+        my_socket.send(bytes('REGISTER sip:'+LINE +' SIP/2.0\r\n\r\n',                                                                                          'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
