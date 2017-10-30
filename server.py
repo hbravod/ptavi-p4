56b1a659c1ff42ba3ecddc9b3fc8e735528d9220
Hugo Bravo Diaz <hbravod@gamma07.aulas.gsyc.urjc.es>
2017-10-30 11:07:41 +0100

    falta pep8 y excepcion

--- server.py
+++ server.py
@@ -4,8 +4,10 @@
 Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
+import json
 import socketserver
 import sys
+import time
 
 
 PORT = int(sys.argv[1])
@@ -18,25 +20,57 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-
     dic_client = {}
+
+    def json2registered(self):
+        try:
+            with open('registered.json', 'r') as archivo:
+                self.dic_client = json.load(archivo)
+                self.expired()
+        except (NameError, FileNotFoundError):
+            pass
+
+    def register2json(self):        
+        self.expired()
+        json.dump(self.dic_client, open('registered.json', "w"))
+
+    def expired(self):
+        expirados = []
+        time_act = time.strftime('%Y-%m-%d %H:%M:%S',time.gmtime(time.time()))
+
+        for usuarios in self.dic_client:
+            if self.dic_client[usuarios][1] < time_act:
+                expirados.append(usuarios)
+        for usuarios in expirados:
+            del self.dic_client[usuarios]
 
     def handle(self):
         """
         handle method of the server class
         (all requests will be handled by this method)
         """
-        self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+        self.json2registered()
+        self.expired()
         for line in self.rfile:
-            if line:
-                if line.decode('utf-8')[:8] == 'REGISTER':
-                    ip_user = line.decode('utf-8')[13:-10]
-                    self.dic_client[ip_user] = self.client_address[0]
-                elif line.decode('utf-8')[:7] == 'Expires':                
-                    if line.decode('utf-8').split(' ')[1][0] == '0':
-                        del self.dic_client[ip_user]
+            mensaje = line.decode('utf-8').split()
+            if mensaje:
+                if mensaje[0] == 'REGISTER':
+                    user = mensaje[1][4:]
+                    direccion = self.client_address[0]
+                if mensaje[0] == 'Expires:':
+                    if mensaje[1] != '0':
+                        expire = time.strftime('%Y-%m-%d %H:%M:%S+%Z', time.gmtime(time.time() + int(mensaje[1])))
+                        self.dic_client[user] = ['address: ' + direccion, 'expires: ' + expire]
+                        self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+                    elif mensaje[1] == '0':
+                        try:
+                            del self.dic_client[user]
+                            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")             
+                        except KeyError:
+                            self.wfile.write(b"SIP/2.0 404 USER NOT FOUND\r\n\r\n")
             print(line.decode('utf-8'), end="")
         print(self.dic_client)
+        self.register2json()
 
 if __name__ == "__main__":
     # Listens at localhost ('') port 6001 
